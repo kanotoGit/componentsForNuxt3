18ea769fd47b65e1dfc0b41d637e4ac83fb109d4
kanotoGit <win.friendof@gmail.com>
2023-04-10 16:45:03 +0900

    textFiledのストーリーブックの修正

--- components/ui/TextField/TextField.stories.ts
+++ components/ui/TextField/TextField.stories.ts
@@ -1,3 +1,4 @@
+import { ref } from 'vue'
 import { Meta, StoryFn } from '@storybook/vue3'
 import TextField from './TextField.vue'
 
@@ -17,9 +18,14 @@ export default {
 const Template: StoryFn<typeof TextField> = args => ({
   components: { TextField },
   setup () {
-    return { args }
+    const text = ref<string | number | null>(null)
+    return { args, text }
   },
-  template: '<TextField v-bind="args" />',
+  template: `
+    <TextField v-model:value="text" v-bind="args" />
+    <div style="margin-top: 10px">出力：{{ String(text) }}</div>
+    <div style="margin-top: 10px">型　：{{ typeof text }}</div>
+  `,
 })
 
 export const basic = Template
